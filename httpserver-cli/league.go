f313e8cbc51f1f291f0b893e61df39b17defc945
umlx5h <umlx5h20@gmail.com>
2022-03-18 08:57:16 +0900

    cli start

---
+++ httpserver-cli/league.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+type League []Player
+
+func NewLeague(rdr io.Reader) (League, error) {
+	var league League
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("probrem parsing league: %w", err)
+	}
+
+	return league, err
+}
+
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+	return nil
+}
---
+++ httpserver-cli/tape_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTape_Write(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	tape := &tape{file}
+
+	tape.Write([]byte("abc"))
+
+	file.Seek(0, 0)
+
+	newFileContents, _ := io.ReadAll(file)
+
+	got := string(newFileContents)
+	want := "abc"
+
+	assert.Equal(t, want, got)
+}
