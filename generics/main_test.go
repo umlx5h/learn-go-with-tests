a753a834567f9f2a69aebbf1648e9980ca7c27bb
umlx5h <umlx5h20@gmail.com>
2022-03-26 11:17:54 +0900

    generics complete

---
+++ generics/main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func TestAssertFunctions(t *testing.T) {
+	t.Run("asserting on integers", func(t *testing.T) {
+		AssertEqual(t, 1, 1)
+		AssertNotEqual(t, 1, 2)
+	})
+
+	t.Run("asserting on stinrgs", func(t *testing.T) {
+		AssertEqual(t, "hello", "hello")
+		AssertNotEqual(t, "hello", "world")
+	})
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func AssertNotEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got == want {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
+
+func AssertTrue(t *testing.T, got bool) {
+	t.Helper()
+	if !got {
+		t.Errorf("got %v, want true", got)
+	}
+}
+
+func AssertFalse(t *testing.T, got bool) {
+	t.Helper()
+	if got {
+		t.Errorf("got %v, want false", got)
+	}
+}
---
+++ generics/stack_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestStack(t *testing.T) {
+	t.Run("integer stack", func(t *testing.T) {
+		myStackOfInts := new(Stack[int])
+		AssertTrue(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(123)
+		AssertFalse(t, myStackOfInts.IsEmpty())
+
+		myStackOfInts.Push(456)
+		value, _ := myStackOfInts.Pop()
+		AssertEqual(t, value, 456)
+		value, _ = myStackOfInts.Pop()
+		AssertEqual(t, value, 123)
+
+		AssertTrue(t, myStackOfInts.IsEmpty())
+	})
+
+	t.Run("string stack", func(t *testing.T) {
+		myStackOfStrings := new(Stack[string])
+
+		// check stack is empty
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+
+		// add a thing, then check it's not empty
+		myStackOfStrings.Push("123")
+		AssertFalse(t, myStackOfStrings.IsEmpty())
+
+		// add another thing, pop it back again
+		myStackOfStrings.Push("456")
+		value, _ := myStackOfStrings.Pop()
+		AssertEqual(t, value, "456")
+		value, _ = myStackOfStrings.Pop()
+		AssertEqual(t, value, "123")
+		AssertTrue(t, myStackOfStrings.IsEmpty())
+	})
+}
